2ae69386574ebc6bb67e8c7f0907ab7abf79de01
AlisonLeiva <alisonleiva24@gmail.com>
2022-02-18 22:44:48 -0600

    .

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -6,7 +6,7 @@ describe('AppComponent', () => {
   it(`should have as title 'Practica2'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('Practica2');
+    expect(app.title).toEqual('Practica3');
   });
 
 });
