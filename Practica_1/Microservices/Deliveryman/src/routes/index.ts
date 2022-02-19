dcc2ad63949671bdc8cd29d15a9c300b3ee5f7e1
AlisonLeiva <alisonleiva24@gmail.com>
2022-02-19 04:06:33 -0600

    remove unnecesary files

--- Practica_1/Microservices/Deliveryman/src/routes/index.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-const router = Router();
-
-import userRoutes from "./deliveryman.route";
-
-router.use('/deliveryman', userRoutes);
-
-export default router;
--- Practica_1/Microservices/Restaurant/src/routes/index.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-const router = Router();
-
-import restaurantRoutes from "./restaurant.route";
-
-router.use('/restaurant', restaurantRoutes);
-
-export default router;
